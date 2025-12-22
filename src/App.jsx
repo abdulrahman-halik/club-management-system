78e121ad9ed2bcfb56694df2a2fea2b5a31c9289
RahmanHalik <abdhurrahman200011@gamil.com>
2025-12-22 08:28:46 +0530

    adding the about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
 import Committee from './pages/Committee';
 import './index.css';
 
@@ -12,7 +13,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="committee" element={<Committee />} />
           {/* Future routes will go here */}
-          <Route path="about" element={<div className="p-8 text-center text-xl">About Page (Coming Soon)</div>} />
+          <Route path="about" element={<About />} />
           <Route path="*" element={<div className="p-8 text-center text-xl">404 - Page Not Found</div>} />
         </Route>
       </Routes>
